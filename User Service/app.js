f1a4eaf20750b17610526729c1725aa89a465511
Harisankar <harisankar2206@gmail.com>
2026-05-11 10:05:29 +0530

    Create app.js

---
+++ User Service/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('User Service Running');
+});
+
+app.listen(3003, () => {
+    console.log('User service running');
+});
